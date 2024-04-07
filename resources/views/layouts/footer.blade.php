83c80f24e9b04b388dcadea0d68304f84df8c6b6
Maximiliano <maximiliano.tedesco@devlights.com>
2024-04-07 00:21:51 -0300

    add adminlte and menu

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+@section('footer')
+    <!-- Main Footer -->
+    <footer class="main-footer">
+        <!-- To the right -->
+        <div class="float-right d-none d-sm-inline">
+        Talentos Digitales
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2024.</strong> Todos los derechos reservados.
+    </footer>
+@endsection
